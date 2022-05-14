3169af3f1bd6b0816f6786def1b0387fde4e8d47
Ball <ball.fiy555@gmail.com>
2022-05-14 17:44:02 +0700

    fix cor

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const cors = require('cors');
 
 
 app.use(cors());
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://fallenangel-bank-api.herokuapp.com/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+  });*/
 app.use(express.json());
 
 
