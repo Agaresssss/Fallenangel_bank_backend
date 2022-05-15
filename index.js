3597005f485166727a1bdb9da420ef6ef50225cb
Ball <ball.fiy555@gmail.com>
2022-05-16 00:50:47 +0700

    fix transaction add report

--- index.js
+++ index.js
@@ -180,7 +180,7 @@ app.post('/customer/currency/balance',(req,res)=>{
 
             const citizenId = req.body.citizenId
                 
-                db.query("SELECT t.transactionId,t.fromAccount,t.toAccount,t.value,t.dateAndTime,t.note FROM `customer-Identification` p,`book-Account` Ba,`transaction`t WHERE p.citizenId = Ba.citizenId AND Ba.accountNum = t.fromAccount AND p.citizenId = ?",
+                db.query("SELECT DISTINCT t.transactionId,t.fromAccount,t.toAccount,t.value,t.dateAndTime,t.note FROM `customer-Identification` p,`book-Account` Ba,`transaction`t WHERE p.citizenId = Ba.citizenId AND Ba.accountNum = t.fromAccount OR Ba.accountNum = t.toAccount AND p.citizenId = ?",
                 [citizenId],(err,result)=>{
                     if(err){
                     console.log(err)
